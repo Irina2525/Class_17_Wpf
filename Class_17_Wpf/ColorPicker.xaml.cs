d01059fe649884db43c88558330bc70fa85b3224
Irina <spartak.irina92@gmail.com>
2022-02-10 23:10:26 +0700

    Добавьте файлы проекта.

---
+++ Class_17_Wpf/ColorPicker.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Class_17_Wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для ColorPicker.xaml
+    /// </summary>
+    public partial class ColorPicker : UserControl
+    {
+
+        public static readonly DependencyProperty ColorProperty;
+        public static readonly DependencyProperty RedProperty;
+        public static readonly DependencyProperty GreenProperty;
+        public static readonly DependencyProperty BlueProperty;
+
+        static ColorPicker()
+        {
+
+            ColorProperty = DependencyProperty.Register(nameof(Color), typeof(Color), typeof(ColorPicker),
+                new FrameworkPropertyMetadata(Colors.Black));
+            RedProperty = DependencyProperty.Register("Red", typeof(byte), typeof(ColorPicker),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+            GreenProperty = DependencyProperty.Register("Green", typeof(byte), typeof(ColorPicker),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+            BlueProperty = DependencyProperty.Register("Blue", typeof(byte), typeof(ColorPicker),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+        }
+
+        public Color Color
+        {
+            get => (Color)GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+        public byte Red
+        {
+            get => (byte)GetValue(RedProperty);
+            set => SetValue(RedProperty, value);
+        }
+        public byte Green
+        {
+            get => (byte)GetValue(GreenProperty);
+            set => SetValue(GreenProperty, value);
+        }
+        public byte Blue
+        {
+            get => (byte)GetValue(BlueProperty);
+            set => SetValue(BlueProperty, value);
+        }
+
+        private static void OnColorRGBChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            ColorPicker colorPicker = (ColorPicker)sender;
+            Color color = colorPicker.Color;
+            if (e.Property == RedProperty)
+                color.R = (byte)e.NewValue;
+            else if (e.Property == GreenProperty)
+                color.G = (byte)e.NewValue;
+            else if (e.Property == BlueProperty)
+                color.B = (byte)e.NewValue;
+
+            colorPicker.Color = color;
+        }
+
+        public ColorPicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
